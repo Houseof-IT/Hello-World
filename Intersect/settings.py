3bee6eae04a8055d2e2e27ae5f16422cdfe46396
Houseof-IT <68476366+Houseof-IT@users.noreply.github.com>
2020-07-26 15:21:22 +0530

    Update settings.py

--- Intersect/settings.py
+++ Intersect/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY='boke$q#_x)w=wb$6&w0ekomz7u3wcra54a#1u=)8jf@r()54%$'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
